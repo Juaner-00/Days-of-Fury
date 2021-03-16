b27fef9f4eae047f0127c9508ab020c747ff3808
Juaner-00 <Juan.ramirezp@upb.edu.co>
2021-03-15 20:23:50 -0500

    Movement PickUP

---
+++ Assets/Scripts/PickUps/MovementSpeedPickUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSpeedPickUp : PickUpBase
+{
+    [SerializeField] float speedPorcentGain;
+
+    protected override void Pick()
+    {
+        GameManager.Player.GetComponent<PlayerMovement>()?.GainSpeed(speedPorcentGain);
+        GameManager.Player.GetComponent<PlayerMovementVels>()?.GainSpeed(speedPorcentGain);
+        base.Pick();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementVels.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovementVels : MonoBehaviour
+{
+    [Header("Script Properties")]
+    [SerializeField] float speedBase;
+    [SerializeField] float rotationTime;
+
+    CharacterController controller;
+
+    float horizontal;
+    float vertical;
+
+    [SerializeField, Header("Debug")]
+    float movementSpeed;
+
+    public static Action OnMoving;
+
+
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        movementSpeed = speedBase;
+    }
+
+    private void Update()
+    {
+        OnMoving?.Invoke();
+
+        HandleInputs();
+        HandleRotation();
+
+        controller.SimpleMove(transform.forward * movementSpeed);
+    }
+
+    void HandleInputs()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void HandleRotation()
+    {
+        if (vertical > 0.1f)
+            transform.DORotate(Vector3.up * 0, rotationTime, RotateMode.Fast);
+        else if (vertical < -0.1f)
+            transform.DORotate(Vector3.up * 180, rotationTime, RotateMode.Fast);
+        else if (horizontal > 0.1f)
+            transform.DORotate(Vector3.up * 90, rotationTime, RotateMode.Fast);
+        else if (horizontal < -0.1f)
+            transform.DORotate(Vector3.up * -90, rotationTime, RotateMode.Fast);
+    }
+
+    // Método para aumentar la velocidad de movimiento
+    public void GainSpeed(float porcent)
+    {
+        movementSpeed += movementSpeed * porcent / 100;
+    }
+}
+
+
+public enum PlayerStates
+{
+    Stoped,
+    SlowAcceleration,
+    FastAcceleration
+}

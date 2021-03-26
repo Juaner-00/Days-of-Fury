32cc9fd2ac47ea6e2e73e7fad1b7a32a868575d8
Dagane <dagane8@hotmail.com>
2021-03-25 20:28:01 -0500

    vfxs
    arreglos

--- Assets/Scripts/Player/ControllPickUps.cs
+++ Assets/Scripts/Player/ControllPickUps.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LifeController : MonoBehaviour
+public class ControllPickUps : MonoBehaviour
 {
-    ParticleSystem vida;
-    ParticleSystem.ShapeModule shapeLife;
-    
     // Start is called before the first frame update
     void Start()
     {
-        vida = GetComponent<ParticleSystem>();
-        shapeLife = vida.shape;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
     }
 }

f38df90d046efdc5f6c3f61c1019a014274e7a04
JoseAgudeloPolygon <jose.magudelo@hotmail.com>
2021-04-18 16:02:25 -0500

    Shooting and Getting Hurt

---
+++ Assets/Scripts/Sounds/EnemyTankSoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTankSoundManager : SoundController
+{
+    EnemyShootController fov;
+    EnemyController controller;
+
+    protected override void ChildAwake()
+    {
+        fov = GetComponent<EnemyShootController>();
+        controller = GetComponent<EnemyController>();
+
+        //fov.OnMoving += PlayMoving;
+        fov.OnShooting += PlayShooting;
+        controller.OnGettingHurt += PlayGettingHurt;
+    }
+
+    private void PlayMoving() {
+        PlaySourceByName("Moving", null, true);
+    }
+    private void PlayShooting()
+    {
+        PlaySourceByName("Shooting");
+    }
+    private void PlayGettingHurt()
+    {
+        PlaySourceByName("GettingHurt");
+    }
+
+    private void OnDisable() {
+        //fov.OnMoving -= PlayMoving;
+        fov.OnShooting -= PlayShooting;
+        controller.OnGettingHurt -= PlayGettingHurt;
+    }
+}

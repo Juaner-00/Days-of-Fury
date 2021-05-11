b2af036cd29ae3eb199a6968115da8fffb9bf8dc
JoseAgudeloPolygon <jose.magudelo@hotmail.com>
2021-05-10 20:26:19 -0500

    Indestructible walls

---
+++ Assets/Scripts/Sounds/IndctBuildingSoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndctBuildingSoundManager : SoundController
+{
+    DestructibleWall wall;
+
+    protected override void SetUp(bool child)
+    {
+        wall = GetComponent<DestructibleWall>();
+
+        SetSubscribeEvents(false);
+        SetSubscribeEvents(true);
+    }
+
+    private void SetSubscribeEvents(bool setSubscribe)
+    {
+        if(setSubscribe)
+        {
+            wall.OnGettingHurt += PlayGettingHurt;
+        }
+        else
+        {
+            wall.OnGettingHurt -= PlayGettingHurt;
+        }
+    }
+
+    private void PlayGettingHurt()
+    {
+        PlayActionByName("GettingHurt");
+    }
+}
